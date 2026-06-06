dab0067ea453b6399171021bccaf84da53caa1b6
Karan Pawar <141235265+thekaranpawar@users.noreply.github.com>
2026-06-07 00:38:56 +0530

    Time: 4 ms (100%), Space: 45.9 MB (71.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if (x < 0) {
+            return false;
+        }
+
+        int rev = 0;
+        int tem = x;
+        while(x != 0){
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x/10;
+        }
+        return rev == tem ;
+    }
+}
