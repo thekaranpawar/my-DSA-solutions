284405391d6988204a6e26233cf600c692935a92
Karan Pawar <141235265+thekaranpawar@users.noreply.github.com>
2026-08-26 21:16:31 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.91%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        
+        char[] ch = s.toCharArray();
+        for(int i=0; i<ch.length; ++i){
+            if (ch[i] >= 'A' && ch[i] <= 'Z'){
+                ch[i] = (char)(ch[i] + 32);
+            }
+        }
+        return new String(ch);
+    }
+}
